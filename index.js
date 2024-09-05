07ed411cfce3205382f4710e3bc49a1ca73ec2a4
Me <123142178+epic-person-on@users.noreply.github.com>
2024-09-05 13:45:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -124,11 +124,8 @@ app.use(express.static(path.join(__dirname, "static")));
 app.use("/ov", cors({ origin: true }));
 
 const routes = [
-  { path: "/as", file: "apps.html" },
-  { path: "/gm", file: "games.html" },
   { path: "/st", file: "settings.html" },
   { path: "/ta", file: "tabs.html" },
-  { path: "/ts", file: "tools.html" },
   { path: "/", file: "index.html" },
   { path: "/tos", file: "tos.html" },
   { path: "/privacy", file: "privacy.html" },
